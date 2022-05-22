84646f82a0f01e5dcb1da57d96879771b5eb5754
bulutluoz <abbulutluoz@gmail.com>
2022-05-22 18:19:25 +0200

    ilk ders sonu

---
+++ src/kisiselKullanim/GithubKullanimi1.java
@@ -0,0 +1,10 @@
+package kisiselKullanim;
+
+public class GithubKullanimi1 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+        System.out.println("That's it");
+    }
+}
