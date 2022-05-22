9d9b5946e9bde8707a731a08d5480b599a716062
bulutluoz <abbulutluoz@gmail.com>
2022-05-22 19:54:44 +0200

    main branch degisiklik

--- src/kisiselKullanim/GithubKullanimi1.java
+++ src/kisiselKullanim/GithubKullanimi1.java
@@ -6,5 +6,7 @@ public class GithubKullanimi1 {
         System.out.println("Hello World");
 
         System.out.println("That's it");
+        System.out.println("main branch degisiklik 1.satir");
+        System.out.println("main branch degisiklik 2.satir");
     }
 }
